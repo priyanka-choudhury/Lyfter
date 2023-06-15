a41d05857fbe5ab480502c33e774ce7e778b36b0
Priyanka Choudhury <124525759+priyanka-choudhury@users.noreply.github.com>
2023-06-15 18:18:33 -0400

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        TestingClient testing = new TestingClient();
+        testing.start();
+
+        System.out.println(" ");
+    }
+}
