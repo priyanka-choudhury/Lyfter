df5a20e6caf4c86b2edb35c81488781f4f5b1ec0
Priyanka Choudhury <124525759+priyanka-choudhury@users.noreply.github.com>
2023-06-15 18:19:16 -0400

    Create Rider.java

---
+++ src/Rider.java
@@ -0,0 +1,10 @@
+public class Rider extends User {
+
+    public Rider(int x, int y, String name) {
+        super(x , y, name);
+    }
+
+    public Rider(String name) {
+        super(name);
+    }
+}
